b206512fbdc0c1503bd9ee968ac9331fa3bb8160
MuhammadAhmedProgrammer <muslimpro.672732@gmail.com>
2025-09-07 10:50:54 +0500

    Music-Player's FRONTEND

--- src/components/PlaylistCard.jsx
+++ src/components/PlaylistCard.jsx
@@ -65,7 +65,7 @@ const PlaylistCard = () => {
           <div
             key={idx}
             onClick={() => setSelectedFolder(item.folder)}
-            className="h-fit max-340:w-[60vw] max-340:ml-2 max-340:mt-2 sm:w-[35vw] lg:w-[40vw] sm:ml-5 xl:w-[190px] bg-[#252525] group hover:bg-[#4b4747] transition-all duration-200 rounded-lg cursor-pointer "
+            className="h-fit max-340:w-[60vw] max-560:w-[70vw] max-560:ml-4 max-340:ml-2 max-340:mt-2 sm:w-[35vw] lg:w-[40vw] sm:ml-5 xl:w-[190px] bg-[#252525] group hover:bg-[#4b4747] transition-all duration-200 rounded-lg cursor-pointer "
           >
             <div className="flex justify-center items-center relative">
               <img
@@ -75,7 +75,7 @@ const PlaylistCard = () => {
                     : "./default-cover.jpeg"
                 }
                 alt="cover"
-                className="xl:w-[190px] xl:h-[165px] p-2 rounded-md w-[78vw] object-cover"
+                className="xl:w-[190px] xl:h-[165px] p-2 rounded-md  w-[78vw] object-cover"
               />
               <img
                 src="./svgs/playbtn.svg"
@@ -84,10 +84,10 @@ const PlaylistCard = () => {
               />
             </div>
             <div className="px-3 pt-2">
-              <h1 className="xl:text-xl text-[55px] truncate max-340:text-[22px] max-560:text-[30px]">
+              <h1 className="xl:text-xl text-[55px] truncate max-340:text-[22px] max-560:text-[25px]">
                 {meta ? meta.title : item.folder}
               </h1>
-              <p className="xl:text-[12px] text-[33px] max-340:text-[18px] max-560:text-[25px] text-gray-400 truncate">
+              <p className="xl:text-[12px] text-[33px] max-340:text-[18px] max-560:text-[22px] text-gray-400 truncate">
                 {meta ? meta.description : "No description"}
               </p>
             </div>
